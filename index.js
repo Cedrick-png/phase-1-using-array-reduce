c9d6ca6ba78eb7335130c4310152b972129fb27a
Cedrickpng <cedrick.burudi@student.moringaschool.com>
2025-03-11 21:54:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+const batteryBatches = [0, 31];
+
+const totalBatteries = batteryBatches.reduce(
+(total, batch) => total + batch,
+0
+);
+
